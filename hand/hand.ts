16217981c383db7806daa498acbb6a16ca6cfaea
古家寧 <11311@uni-psg.com>
2023-05-23 10:13:02 +0800

    feat: hand→移動 hand 並添加 drawCard 方法

---
+++ hand/hand.ts
@@ -0,0 +1,37 @@
+import { Card } from '../card';
+
+export abstract class Hand {
+  private _cards: Card[] = [];
+
+  size: number = 0;
+
+  constructor() {}
+
+  abstract showCard(): Promise<Card>;
+
+  getSize() {
+    return this.cards.length;
+  }
+
+  public get cards(): Card[] {
+    return this._cards;
+  }
+  public set cards(value: Card[]) {
+    if (this.cards.length == 4) {
+    }
+    this._cards = value;
+  }
+
+  public addCard(card: Card) {
+    this._cards.push(card);
+  }
+  // 抽出手牌
+  public drawCard(index: number) {
+    const drawnCard = this.cards[index];
+
+    // 移除抽到的牌
+    this.cards.splice(index, 1);
+
+    return drawnCard;
+  }
+}
