e2030618c01d485f77270e03eb5c257ec91de9a2
charlie50503 <cate50503@gmail.com>
2023-05-23 23:07:26 +0800

    feat: hand/HumanHand→修改 viewCard function name

--- hand/humanHand.ts
+++ hand/humanHand.ts
@@ -5,9 +5,9 @@ import { Hand } from './hand';
 export class HumanHand extends Hand {
   // showCard by commandline input
   showCard(): Promise<Card> {
-    this.viewCard();
+    this.viewCards();
     return new Promise((resolve, reject) => {
-      rl.question(`請選擇要出的牌 : `, (index) => {
+      rl.question(`請選擇要出的牌 : `, (index:string) => {
         if (isNaN(Number(index))) {
           return reject('請輸入數字');
         } else if (Number(index) > this.cards.length - 1) {
@@ -18,7 +18,7 @@ export class HumanHand extends Hand {
     });
   }
 
-  viewCard(): void {
+  viewCards(): void {
     this.cards.forEach((card, index) => {
       console.log(
         `手牌編號:${index}, 花色:${card.suit.type}, 數字:${card.rank.type}`
