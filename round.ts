101c6d237ac3a265a3857ce980f238e159a39bfc
古家寧 <11311@uni-psg.com>
2023-05-23 10:15:51 +0800

    feat: round→補齊 round 內容

--- round.ts
+++ round.ts
@@ -1,11 +1,36 @@
-import { Card } from "./card";
+import { Card } from './card';
+
+export interface playCard {
+  playerId: number;
+  card: Card;
+}
 
 export class Round {
-  cards !:Card[];
+  playCards: playCard[] = [];
+  roundIndex: number = 0; // 每一局內執行到哪個玩家
 
-  playCards!:[]
-  roundIndex:number = 0; // 每一局內執行到哪個玩家
-  nextPlayer(){
+  addPlayCard(playCard: playCard) {
+    this.playCards.push(playCard);
+  }
+  nextPlayer() {
     this.roundIndex++;
+  }
+  sumPoints(card: Card) {
+    return card.rank.value + card.suit.value;
+  }
+  showdown() {
+    let maxPointPlayer = {
+      playerId: -1,
+      point: -1,
+    };
+    this.playCards.forEach((playCard) => {
+      if (maxPointPlayer.point < this.sumPoints(playCard.card)) {
+        maxPointPlayer = {
+          playerId: playCard.playerId,
+          point: this.sumPoints(playCard.card),
+        };
+      }
+    });
+    return maxPointPlayer.playerId
   }
 }
